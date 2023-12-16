814d9ec85114120c7867b4f78bef693c9ca2e88d
Яна <106766066+cherrryana@users.noreply.github.com>
2023-12-16 18:28:25 +0200

    Delete UnitConverter directory

--- UnitConverter/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.unitconverter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.unitconverter to javafx.fxml;
-    exports com.example.unitconverter;
-}
